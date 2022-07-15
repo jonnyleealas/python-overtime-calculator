e4f489187a91e8ed462045a7643872d3ae2e3dee
Jonathon Lee <jonnyleealas@gmail.com>
2022-07-15 10:00:31 -0500

    file path

--- payCalculator.py
+++ payCalculator.py
@@ -1,10 +1,14 @@
+from asyncore import write
+
+
 def computePay(hours, rate):
     regularPay = rate * 40
     overTimeHours = hours - 40
     timeAndAHalf = rate * 1.5
+    print(timeAndAHalf, 'i am time and a half')
     overTime = overTimeHours * timeAndAHalf
     # format is used to add 2 decimal places to our float
-    overTimePay = format(float(overTime + regularPay),'.2f')
+    overTimePay = format(float(overTime + regularPay), '.2f')
     nonOverTimePay = format(float(hours * rate), '.2f')
     if hours > 40:
         print(overTimePay)
@@ -12,11 +16,13 @@ def computePay(hours, rate):
     else:
         print(nonOverTimePay)
         return '${}'.format(nonOverTimePay)
+
+
 # save our compute function for later use
-compute = computePay(49.30, 10.23)
+compute = computePay(41, 10)
 # open the file with append option 'a'
-f = open('payStub.txt', 'a')
+f = open('/Users/jonnylee/pythonTextFile.txt', 'a')
 # use writelines to write to our file and convert to a string because the function only writes strings
-with open('payStub.txt', 'a') as f:
+with open('/Users/jonnylee/pythonTextFile.txt', 'a') as f:
     f.writelines(str(compute))
     f.write('\n')
